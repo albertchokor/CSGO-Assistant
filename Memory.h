9fbd01723e7224f256b13748761cfa1904760681
Albert Cho <albert.cho.kor@gmail.com>
2020-05-25 00:40:48 -0700

    Added Constructors/Destructors; Fixed Memory Leaks; Finished memory manager.

--- Memory.h
+++ Memory.h
@@ -5,6 +5,7 @@
 #include <TlHelp32.h> //Contains functions useful for retrieving modules (.dll) from the game.
  
 class Memory { // Handles game memory process.
+
     private:
         DWORD dwPID; // ProcessID of game process.
         HANDLE hProc; // Handle of game process.
@@ -67,6 +68,51 @@ class Memory { // Handles game memory process.
 	return WriteProcessMemory(hProcess, (LPVOID)dwAddress, &ValueToWrite, sizeof(c), NULL);
 	}
 
-	// Fix possible memory leaks, write constructor/deconstructors, getters for instance vars
+	DWORD GetProcID() { // Getter for PID.
+		return this->dwPID; 
+	}
+	HANDLE GetProcHandle() { // Getter for Process Handle.
+		return this->hProc; 
+	}
 
+	Memory() { // Memory Manager Constructor.
+		this->hProc = NULL;
+		this->dwPID = NULL;
+		try { // Retrieves memory process and modules of running game.
+			if (!AttachProcess("csgo.exe")) throw 1;
+			this->ClientDLL = GetModule("client.dll");
+			this->EngineDLL = GetModule("engine.dll");
+			this->ClientDLL_Base = (DWORD)this->ClientDLL.modBaseAddr;
+			this->EngineDLL_Base = (DWORD)this->EngineDLL.modBaseAddr;
+			if (this->ClientDLL_Base == 0x0) throw 2;
+			if (this->EngineDLL_Base == 0x0) throw 3;
+			this->ClientDLL_Size = this->ClientDLL.dwSize;
+			this->EngineDLL_Size = this->EngineDLL.dwSize;
+		} catch (int iEx) { // Close program if error conditions met.
+			switch (iEx)
+			{
+			case 1:
+				MessageBoxA(NULL, "CS:GO not running.", "ERROR", MB_ICONSTOP | MB_OK); 
+				exit(0); 
+				break;
+			case 2: 
+				MessageBoxA(NULL, "No Client.dll", "ERROR", MB_ICONSTOP | MB_OK); 
+				exit(0); 
+				break;
+			case 3: 
+				MessageBoxA(NULL, "No Engine.dll", "ERROR", MB_ICONSTOP | MB_OK); 
+				exit(0); 
+				break;
+			default: 
+				throw;
+			}
+		} catch (...) {
+			MessageBoxA(NULL, "Unhandled Exception", "ERROR", MB_ICONSTOP | MB_OK);
+			exit(0);
+		}
+	}
+
+	~Memory() { // Memory Manager Destructor to close process handle to not lie in memory anymore.
+		CloseHandle(this->hProcess);
+	}
 };
