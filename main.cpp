cac483bf93a4d07d9d702a152daf0ee98c9ef788
Albert Cho <albert.cho.kor@gmail.com>
2020-05-26 23:14:46 -0700

    Added Toggleable AutoHop Function; Started Aimbot Function.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
  
 Memory* Mem; // Execute memory manager in main entry point.
  
-// Offsets for future/other functions.
+// Offsets for current and future functions.
 #define localPlayer = 0xCBD6B4;
 #define entityList = 0x4CCDBFC;
 #define iTeamNum = 0xF4;
 #define bSpotted = 0x93D;
-#define iGlowIndex = 0xA3F8;
+#define iGlowIndex = 0xA3F8; // Glow ESP function perhaps using in game feature.
 #define objectGlowManager = 0x520DA28;
 #define flashDuration = 0xA3E0;
 #define modelAmbientMin = 0x58ED1C;
@@ -28,23 +28,25 @@ Memory* Mem; // Execute memory manager in main entry point.
 #define position = 0x138;
 #define bDormant = 0xED;
 
-// TriggerBot Offsets.
+// Offsets for TriggerBot.
 #define iCrossHairID 0xA940
 
-// No Flash Offset.
+// Offset for No Flash.
 #define flashDuration = 0xA3F4;
+
+// Offsets for Bhop.
+#define entityAutoJump = 0x51A81BC;
+#define entityJumpState = 0x104;
 
 struct Vec2 { // Vec2 stores screen 2D x, y coordinates.
     float x, y;
 };
 
-struct Vec3 // Vec3 stores player position.
-{
+struct Vec3 { // Vec3 stores player position.
     float x, y, z;
 };
 
-struct Vec4 // Vec4 stores clip coordinates.
-{
+struct Vec4 { // Vec4 stores clip coordinates.
     float x, y, z, w;
 };
 
@@ -225,15 +227,13 @@ void noFlash() { // Simple No Flash Function (No Player Blinding from Flash Util
 // To press key and toggle triggerbot function on/off.
 boolean triggerToggled = false;
 
-// Trigger Bot Main Function: Attack automatically if enemy is in player's crosshair.
-void triggerBot() {
+void triggerBot() { // Trigger Bot Main Function: Attack automatically if enemy is in player's crosshair.
     // Check for toggle on/off.
     if (GetAsyncKeyState(VK_F1)) { // If pressed F1 key, toggle on/off.
         triggerToggled = !triggerToggled;
         Sleep(200); // So pressing key doesn't switch on/off the moment it presses.
     }
-    // If the triggerbot toggled, proceed triggerbot.
-    if (triggerToggled) {
+    if (triggerToggled) { // If triggerbot toggled, proceed triggerbot.
         // Retrieve player information.
         DWORD LocalPlayer_Base = Mem->Read<DWORD>(Mem->ClientDLL_Base + m_dwLocalPlayer);
         int localPlayerInCross = Mem->Read<int>(LocalPlayer_Base + main.iCrossHairID); // Checks entity in player's crosshair.
@@ -253,15 +253,47 @@ void triggerBot() {
     }
 }
 
+// To press key and toggle bhop function on/off.
+boolean autoHopToggled = false;
+
+void autoHop() { // AutoHop Function: Automatically jumps/bhops when holding space bar done if toggled on.
+    // Check for toggle on/off.
+    if (GetAsyncKeyState(VK_F4)) { // If pressed F4 key, toggle on/off.
+        autoHopToggled = !autoHopToggled;
+        Sleep(200); // So pressing key doesn't switch on/off the moment it presses.
+    }
+    if(autoHopToggled){ // If bhop toggled, proceed bhop function.
+        BYTE autoJump = 6; // Represents forced jump interval: autoJump: 4 = On Ground, 5 = Jumping, 6 = Reset to 4 to be able to jump again.
+        int flags; // Represents player state: flags: 257 = On Ground, 256 = Jumping.
+        DWORD myEntity;
+        ReadProcessMemory(Mem.getProcHandle(), (LPVOID)(Mem.ClientDLL + main.localPlayer), &myEntity, sizeof(myEntity), 0);
+        if (myEntity == NULL) {
+            while (myEntity == NULL) {
+                ReadProcessMemory(Mem.getProcHandle(), (LPVOID)(Mem.ClientDLL + main.localPlayer), &myEntity, sizeof(myEntity), 0);
+            }
+        }
+        ReadProcessMemory(Mem.getProcHandle(), (PBYTE)(myEntity + main.entityJumpState), &flags, sizeof(flags), 0);
+        if (GetAsyncKeyState(VK_SPACE) && flags & (1 << 0)) { // Checks if space bar is held and swaps player state to jump.
+            WriteProcessMemory(Mem.getProcHandle(), (PBYTE)(Mem.ClientDLL + main.entityAutoJump), &autoJump, sizeof(autoJump), 0);
+            Sleep(1); // Jump on tickrate intervals, Change this depending on server tickrate to work properly.
+        }
+    }
+}
+
+void aimbot() { // Aimbot Function: Automatically assist player in aiming crosshair to enemy.
+    // Check enemy in view, Calculate angles to enemy bones, Shift mouse to target bone, Track on screen unless toggled off.
+}
+
 int main() { // Main process.
     SetConsoleTitleA("CSGO Assistant");
     Mem = new MemoryManager();
     esp.game = FindWindowA(0, "Counter-Strike: Global Offensive"); // Gets game window for ESP feature.
     esp.gameWindow = GetDC(esp.game);
-    while (Mem.getProcID() != NULL) { // Runs playerESP, noFlash, and triggerBot that user can toggle on and off until game application closes.
+    while (Mem.getProcID() != NULL) { // Runs playerESP, noFlash, triggerBot, and autoHop that the user can toggle on and off until game application closes.
         playerESP();
         noFlash();
         triggerBot();
+        autoHop();
     }
     CloseHandle(Mem.getProcHandle());
     delete Mem; // Deletes Memory Manager pointer to execute destructor/close hProc.
